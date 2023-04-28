4595ec9f5a66898c943a7e507b7bb228bb0641be
chiba <alaa.chiba@esprit.tn>
2023-04-28 13:08:27 +0100

    final version

--- check_dom/index.js
+++ check_dom/index.js
@@ -1,16 +1,13 @@
 var buttonPlus=Array.from(document.getElementsByClassName("add"))
-var price=Array.from(document.getElementsByClassName("price"))
 var total=document.getElementById("total")
-var totalValue=0
+
+
 
 
 for(let i=0;i<buttonPlus.length;i++){
   buttonPlus[i].addEventListener("click",function(){
     buttonPlus[i].nextElementSibling.innerHTML++
-    let priceValue = parseInt(price[i].innerHTML)
-    console.log(priceValue,"vjbo")
-    totalValue += priceValue;
-    total.innerHTML = totalValue;
+    shopping_bag()
   })
 }
 
@@ -20,9 +17,7 @@ for(let i=0;i<buttonMinus.length;i++){
   buttonMinus[i].addEventListener("click",function(){
     if(buttonMinus[i].previousElementSibling.innerHTML >0){
     buttonMinus[i].previousElementSibling.innerHTML--
-    let priceValue = parseInt(price[i].innerHTML)
-    totalValue -= priceValue;
-    total.innerHTML = totalValue;
+    shopping_bag()
     }
   })
 }
@@ -31,13 +26,24 @@ var trash=Array.from(document.getElementsByClassName("fa-trash-alt"))
 for(let i=0;i<trash.length;i++){
   trash[i].addEventListener("click",function(){
     trash[i].parentNode.remove()
+    shopping_bag()
   })
+  
 }
 
 var heart=Array.from(document.getElementsByClassName("fa-heart"))
 for(let i=0;i<heart.length;i++){
-    heart[i].addEventListener("click", () => {
+    heart[i].addEventListener("click", function() {
         heart[i].classList.toggle("red");
       });
 }
 
+function shopping_bag(){
+  var quantite=Array.from(document.getElementsByClassName("qte"))
+  var price=Array.from(document.getElementsByClassName("price"))
+  var totalValue=0
+  for(let i=0;i<quantite.length;i++){
+    totalValue += price[i].innerHTML * quantite[i].innerHTML
+  }
+  return total.innerHTML = totalValue
+}
